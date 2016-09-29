6daa22c8d4592a7bd24d659ad02d2e50eb87b078
Maikki Inkubiitti <maikki@makaroni.kyla.fi>
2016-09-29 21:48:52 +0300

    Functionality for server-side picture fetching

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
 from uuid import uuid4
-
 import re
-
 from telegram import InlineQueryResultArticle, InlineQueryResultPhoto, ParseMode, \
     InputTextMessageContent
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import logging
 import urllib.request
+import os
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 
+def _get_img_from_kiltiscam():
+    """
+    Grabs current kiltiscam image from server
+    """
+    cur_path = os.path.dirname(__file__)
+    img_path = os.path.relpath("../kiltiscam/kiltahuone.jpg")
+    f = open(img_path, "rb")
+    return f
+
+
 def _get_img_from_url(url):
+    """
+    Deprecated old function that grabs the image from
+    inkubio.fi/kiltiscam/kiltahuone.jpg
+    """
     img = urllib.request.urlopen(url).read()
     with open('out.jpg','wb') as f:
         f.write(img)
         i = open('out.jpg', 'rb')
         return i
-        
+
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Hi!')
-
-
-def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Help!')
-
 
 def stalk(bot, update):
-
+    """
+    Command to post current kiltiscam image
+    """
     msg = update.message.text.lower()
-    if msg == '/stalk' or msg == '/stalk@kahmybot':
-        url = 'http://www.inkubio.fi/kiltiscam/kiltahuone.jpg'
-        img = _get_img_from_url(url)
+    if msg == '/stalk' or msg == '/stalk@kiltisbot':
+        img = _get_img_from_kiltiscam()
         bot.sendPhoto(update.message.chat_id, photo=img)
         img.close()
-        #resp = multipart.post_multipart(BASE_URL + 'sendPhoto', [('chat_id', str(chat_id))], [('photo', 'image.jpg', img)])
 
-
+# Experimental functionality, currently disabled
+"""
 def inlinequery(bot, update):
     query = update.inline_query.query
-    results = list()
+    results = []
 
     results.append(InlineQueryResultPhoto(id=uuid4(),
                                             photo_url="http://www.inkubio.fi/kiltiscam/kiltahuone.jpg",
@@ -56,9 +62,8 @@ def inlinequery(bot, update):
                                             photo_url="http://ruutu.hut.fi/pics/30-Jonotilanne.jpg",
                                             thumb_url="http://ruutu.hut.fi/pics/30-Jonotilanne.jpg"))
 
-
     bot.answerInlineQuery(update.inline_query.id, results=results)
-
+"""
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -66,23 +71,19 @@ def error(bot, update, error):
 
 def main():
     # Create the Updater and pass it your bot's token.
-    kahmytoken="163712304:AAHznhUn9ajhmBHjrOJDVhWNMBQxM89ROoU"
-    kiltistoken="69427851:AAFl1pyuTLjYz3OOIIiBEmM_QTnoOgtpZbU"
 
     # https://kiltisbot.appspot.com/set_webhook?url=https://kiltisbot.appspot.com/webhook
 
-    updater = Updater(kahmytoken)
+    updater = Updater(config.kiltistoken)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("stalk", stalk))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(InlineQueryHandler(inlinequery))
+    # dp.add_handler(InlineQueryHandler(inlinequery))
 
     # log all errors
     dp.add_error_handler(error)
