f9256ea2190e232a7cf2e8c1c32945a544cc2025
jonesus <joonas.palosuo@gmail.com>
2016-10-02 12:19:47 +0300

    Working implementation of /addquote

--- kiltisbot.py
+++ kiltisbot.py
@@ -1,12 +1,20 @@
 from uuid import uuid4
 import re
-from telegram import InlineQueryResultArticle, InlineQueryResultPhoto, ParseMode, \
+from telegram import InlineQueryResultArticle, \
+    InlineQueryResultPhoto, \
+    ParseMode, \
     InputTextMessageContent
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext import Updater, \
+    InlineQueryHandler, \
+    CommandHandler, \
+    MessageHandler, \
+    Filters
 import logging
 import urllib.request
 import os
 import config
+import sqlite3
+from datetime import datetime
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,6 +22,15 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+def _init_quote_db():
+    """
+    Initializes database connection
+    Returns cursor to interact with db
+    """
+    connection = sqlite3.connect("quote.db")
+    return connection, connection.cursor()
+
+'''
 def _get_img_from_kiltiscam():
     """
     Grabs current kiltiscam image from server
@@ -35,7 +52,7 @@ def _get_img_from_url(url):
         i = open('out.jpg', 'rb')
         return i
 
-
+'''
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 
@@ -43,11 +60,48 @@ def stalk(bot, update):
     """
     Command to post current kiltiscam image
     """
-    msg = update.message.text.lower()
-    if msg == '/stalk' or msg == '/stalk@kiltisbot':
-        img = _get_img_from_kiltiscam()
-        bot.sendPhoto(update.message.chat_id, photo=img)
-        img.close()
+    img = _get_img_from_kiltiscam()
+    bot.sendPhoto(update.message.chat_id, photo=img)
+    img.close()
+
+
+def add_quote(bot, update):
+    """
+    Adds a quote from a telegram chat to a chat-specific database.
+    User needs to reply to a message he wants to quote with the message
+    '/addquote'
+    """
+    if not update.message.reply_to_message:
+        bot.sendMessage(update.message.chat.id,
+                        "Please use '/addquote' by replying to a message.",
+                        reply_to_message_id=update.message.message_id)
+        return
+
+    message = update.message
+    reply = message.reply_to_message
+    quote = reply.text
+    message_id = reply.message_id
+    chat_id = reply.chat.id
+    said_by = reply.from_user.first_name + " " + reply.from_user.last_name
+    added_by = message.from_user.first_name + " " + message.from_user.last_name
+    date_added = int(message.date.timestamp())
+    date_said = int(reply.date.timestamp())
+
+    conn, c = _init_quote_db()
+    try:
+        c.execute("INSERT INTO quotes VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (quote, message_id, chat_id, said_by, added_by, date_said, date_added))
+        conn.commit()
+        bot.sendMessage(chat_id, "Quote added!")
+    except Exception as e:
+        bot.sendMessage(chat_id, "Error adding quote:\n{}".format(e))
+    finally:
+        conn.close()
+
+
+def get_quote(bot, update):
+    pass
+
 
 # Experimental functionality, currently disabled
 """
@@ -72,21 +126,22 @@ def error(bot, update, error):
 
 def main():
     # Create the Updater and pass it your bot's token.
-
-    # https://kiltisbot.appspot.com/set_webhook?url=https://kiltisbot.appspot.com/webhook
-
     updater = Updater(config.kahmytoken)
+
+    # List for pending quote additions
+    global quote_queue
+    quote_queue = {}
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("stalk", stalk))
+    dp.add_handler(CommandHandler("addquote", add_quote))
 
     # on noncommand i.e message - echo the message on Telegram
     # dp.add_handler(InlineQueryHandler(inlinequery))
 
-    # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
